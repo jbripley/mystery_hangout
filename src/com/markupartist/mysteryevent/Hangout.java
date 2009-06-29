b427532b30b6f4ebb060d29027197082c86e6f4a
Johan Nilsson <github@markupartist.com>
2009-06-29 16:29:16 +0200

    hangout representation

---
+++ src/com/markupartist/mysteryevent/Hangout.java
@@ -0,0 +1,33 @@
+package com.markupartist.mysteryevent;
+
+public class Hangout {
+    private String title;
+    private String latitude;
+    private String longitude;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    
+}
