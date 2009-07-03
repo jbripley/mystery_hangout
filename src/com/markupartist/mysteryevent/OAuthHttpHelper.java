9a5cdb33f20f4f6185dfc6d789d46f2880eea415
johann <johann@turokhan>
2009-07-03 13:24:22 +0200

    Replaced tabs with spaces.

--- src/com/markupartist/mysteryevent/OAuthHttpHelper.java
+++ src/com/markupartist/mysteryevent/OAuthHttpHelper.java
@@ -23,28 +23,28 @@ import android.util.Log;
  * Wrapper around HTTPClient and Sign post that sign request and sends them.
  */
 public class OAuthHttpHelper {
-	private static String TAG = "OAuthHttpHelper";
+    private static String TAG = "OAuthHttpHelper";
     private DefaultHttpClient mHttpClient = new DefaultHttpClient();
     private OAuthConsumer mConsumer;
 
     public OAuthHttpHelper(String consumerKey, String consumerSecret) {
-        mConsumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret, 
+        mConsumer = new CommonsHttpOAuthConsumer(consumerKey, consumerSecret,
                 SignatureMethod.HMAC_SHA1);
     }
 
     /**
-     * Posts provided parameters to the provided url.
+     * Posts provided parameters to the provided url. 
      * TODO: Need to add a communication exception or something like that.
      * @param url the url to post to
      * @param params list of parameters
      * @return response of the post
      */
     public InputStream post(String url, List<NameValuePair> params) {
-    	HttpPost httpPost = new HttpPost(url);
+        HttpPost httpPost = new HttpPost(url);
         InputStream responseStream = null;
         try {
-        	UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params, "UTF-8");
-        	httpPost.setEntity(entity);
+            UrlEncodedFormEntity entity = new UrlEncodedFormEntity(params, "UTF-8");
+            httpPost.setEntity(entity);
 
             mConsumer.sign(httpPost);
 
@@ -53,11 +53,11 @@ public class OAuthHttpHelper {
         } catch (ClientProtocolException e) {
             Log.e(TAG, e.getMessage());
         } catch (IOException e) {
-        	Log.e(TAG, e.getMessage());
+            Log.e(TAG, e.getMessage());
         } catch (OAuthMessageSignerException e) {
-        	Log.e(TAG, e.getMessage());
+            Log.e(TAG, e.getMessage());
         } catch (OAuthExpectationFailedException e) {
-        	Log.e(TAG, e.getMessage());
+            Log.e(TAG, e.getMessage());
         }
 
         return responseStream;
